9a9bd62b3f8c12926ad8f7b74b217571ea0bd46b
Vinhthuba <100991117+Vinhthuba@users.noreply.github.com>
2022-08-06 19:36:27 -0700

    Add project files.

---
+++ GeometryExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cy = new Cylinder();
+        cy.Result();
+
+
+      
+
+        cy.Process();
+    }
+}
